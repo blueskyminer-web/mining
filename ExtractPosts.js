aea325f9177ac558cdacf7bc2a0869d8096a8402
blueskyminer-web <blueskyminer2@gmail.com>
2026-04-03 09:14:22 +0100

    Add files via upload
    extract posts sub section

---
+++ ExtractPosts.js
@@ -0,0 +1,146 @@
+ // global variables 
+ //   let IndexYear = "2026";
+
+  
+    let currentMonth = "";
+    let currentYear = "";
+    let PostTitles = new Array();
+    let PostURLs = new Array();
+    let PostYears = new Array();
+    let PostMonths = new Array();
+    let PostDays = new Array();
+    let PostTags = new Array();
+    let PostWalk = new Array();
+    let PostSummary = new Array();
+  
+  function LoadTheArchive(TotalFeed)
+{
+    if("entry" in TotalFeed.feed)
+    {
+      let PostEntries=TotalFeed.feed.entry.length;
+    for(var PostNum=0; PostNum<PostEntries ; PostNum++)
+      {
+      let ThisPost = TotalFeed.feed.entry[PostNum];
+      let AddThis = "Yes"; //so we can filter out some posts!
+
+// loop around the URLs to get the one we want
+      let ThisPostURL;
+      for(let LinkNum=0; LinkNum < ThisPost.link.length; LinkNum++)
+        {
+          if(ThisPost.link[LinkNum].rel == "alternate")
+          {
+             ThisPostURL = ThisPost.link[LinkNum].href;
+             break
+          }
+        }
+
+        // shorten the summary if a more tag is found
+        let ThisSummaryFull=ThisPost.summary.$t;
+        let ThisSummaryShort=ThisSummaryFull;
+         if (ThisSummaryFull.includes("//")) {
+             ThisSummaryShort = ThisSummaryFull.substring(0,ThisSummaryFull.indexOf("//"))
+             }
+         else if (ThisSummaryFull.includes(".")) {
+              ThisSummaryShort = ThisSummaryFull.substring(0,ThisSummaryFull.lastIndexOf(".")+1)
+                  }
+        
+//        if(ThisSummaryFull.search("more")>0)
+//        {
+//          ThisSummaryShort = "shortned" + ThisSummaryFull.search("--more--")
+//        }
+        
+        // loop around the categories (tags) to get them all in one field. And also manipulate the marker ones.
+        let ThisPostTags = "";
+        let ThisWalkTag="";
+        let ThisTag="";
+        let NumTags=ThisPost.category.length;
+        
+        for(let TagNum=0; TagNum < NumTags; TagNum++)
+        {
+         ThisTag=ThisPost.category[TagNum].term;
+         if (ThisTag == "@Day")
+         {
+           ThisWalkTag="Solo Walk";
+         } 
+          else if (ThisTag == "@Meet")
+       {
+         ThisWalkTag="Meet Walk";
+       } 
+          else if (ThisTag == "@Group")
+       {
+         ThisWalkTag="Group Walk";
+       } 
+          else if (ThisTag == "@Break")
+       {
+         ThisWalkTag="Short Break Walk";
+       } 
+          else if (ThisTag == "@Trip")
+       {
+         ThisWalkTag="Trip Walk";
+       } 
+          else if (ThisTag == "@Work")
+       {
+         ThisWalkTag="Work Walk";
+       } 
+          else if (ThisTag == "Summary")
+       {
+         AddThis="No";
+       } 
+          else if (ThisTag.substring(0,1) == "*")
+       {
+ ///  ignore
+       } 
+//          else if (ThisTag == "2026")
+//          else if (ThisTag == IndexYear)
+          else if (ThisTag > 1959)
+       {
+ ///  ignore as in the search criteria
+       } 
+          else if (ThisTag == "Walk")
+       {
+ ///  ignore as in the Search Criteria
+       }
+          else if (ThisTag == "AAC")
+       {
+ ///  ignore as group name
+       }
+          else if (ThisTag == "BOC")
+       {
+ ///  ignore as group name
+       }
+          else if (ThisTag == "WACT")
+       {
+ ///  ignore as group name
+       } 
+          else
+         {
+
+          if (ThisPostTags=="")
+           {
+             ThisPostTags=ThisTag;
+           }
+          else
+           {
+             ThisPostTags = ThisPostTags + "," + ThisTag;
+           }
+         }
+
+       }
+
+        if (AddThis=="Yes")
+        {
+      PostTitles.push(ThisPost.title.$t);
+      PostYears.push(ThisPost.published.$t.substring(0,4));
+      PostMonths.push(ThisPost.published.$t.substring(5,7));
+      PostDays.push(ThisPost.published.$t.substring(8,10));
+      PostURLs.push(ThisPostURL);
+      PostTags.push(ThisPostTags);
+      PostWalk.push(ThisWalkTag);
+   	  PostSummary.push(ThisSummaryShort);
+        }
+        
+        
+   // loop around posts until finished
+      }
+    }
+}
